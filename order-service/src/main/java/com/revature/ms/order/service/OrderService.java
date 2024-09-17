a8699e9cc83e13a4a142f977b6444b7ee87f56c5
Jasdhir Singh <jasdhir.singh@revature.com>
2024-09-17 08:36:59 +0530

    Feign Client

--- order-service/src/main/java/com/revature/ms/order/service/OrderService.java
+++ order-service/src/main/java/com/revature/ms/order/service/OrderService.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.revature.ms.order.client.InventoryClient;
 import com.revature.ms.order.dto.OrderRequest;
 import com.revature.ms.order.model.Order;
 import com.revature.ms.order.repository.OrderRepository;
@@ -17,17 +18,22 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
    
     public void placeOrder(OrderRequest orderRequest) {
 
-       
+       var isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+       if(isInStock) {
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
             order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
-
+       }else {
+    	   throw new RuntimeException("Product with skuCode "+orderRequest.skuCode()+" is not in stock");
+       }
+          
            
     }
 }
--- readme.txt
+++ readme.txt
@@ -7,3 +7,11 @@
 3. Inventory Service - Branch inventoryService
 	Inventory Service RestAPI created
 	
+4. Feign Client - Branch feignClient
+	FeignClient is a Web service client
+	we will create a client in the order service which will call the inventory service
+	create the client interface in the client package and update the service class in 
+	order service to call the inventory service and test.
+	Add the feign dependency
+	don't forget to enable the FeignClient @EnableFeignClients in main of order-service
+	Test your application order-service using post main	
